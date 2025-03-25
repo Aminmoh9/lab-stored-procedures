0f266371611a4a3a05dae62ec6577b494971598a
Aminmoh9 <amin.moh2017@gmail.com>
2025-03-25 14:42:43 +0100

    Minor changes.

--- lab-stored-procedures.sql
+++ lab-stored-procedures.sql
@@ -34,7 +34,7 @@ BEGIN
 END //
 DELIMITER ;
 
-call action_film_customers();
+CALL action_film_customers();
 -- Now keep working on the previous stored procedure to make it more dynamic. 
 -- Update the stored procedure in a such manner that it can take a string argument for the category name and return the results 
 -- for all customers that rented movie of that category/genre. For eg., it could be action, animation, children, classics, etc.
@@ -52,7 +52,7 @@ BEGIN
     GROUP BY first_name, last_name, email;
 END //
 DELIMITER ;
-call customers_by_category('Family');
+CALL customers_by_category('Family');
 
 -- Write a query to check the number of movies released in each movie category. 
 -- Convert the query in to a stored procedure to filter only those categories that have movies released greater than a certain number. 
